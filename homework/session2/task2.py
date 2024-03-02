661582f417a604fce212490e72945908f2eddffa
maidieva <meeirmaidieva@gmail.com>
2024-03-02 15:48:49 +0000

    homework

---
+++ homework/session2/task2.py
@@ -0,0 +1,4 @@
+inp=(input("Enter your sentence: "))
+last_word=inp[inp.rfind (" ") + 1:]
+length_of_the_last_word=len(last_word)
+print("The last word is ",length_of_the_last_word, "charachters long")
---
+++ homework/session2/task3.py
@@ -0,0 +1,5 @@
+x=(input("Enter value: "))
+if str(x) == str(x)[::-1]:
+    print("value is a palindrome")
+else:
+    print("value is not a palindrome")
